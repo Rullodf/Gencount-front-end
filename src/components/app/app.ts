21cd9741e5cec9772da8ebaf3914fde14cc5cbda
Rullodf <defrancescolorenzo09@gmail.com>
2026-03-03 18:07:59 +0100

    robe

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ThemeService} from '../services/theme.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styles: `
+    div{
+        background-color: var(--md-sys-color-primary);
+    }
+  `
 })
 export class App {
+  constructor(private themeService: ThemeService) {
+  }
   protected readonly title = signal('Gencount');
 }

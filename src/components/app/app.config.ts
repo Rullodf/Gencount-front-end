d36a840a8557b7eb232699a1f4467330f3f22d15
Rullodf <defrancescolorenzo09@gmail.com>
2026-03-05 13:06:35 +0100

    boh

--- src/components/app/app.config.ts
+++ src/components/app/app.config.ts
@@ -4,13 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from '../../interceptor/authInterceptor';
-import {CircleUserRound, LucideAngularModule, UserRoundPlus} from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-    importProvidersFrom(LucideAngularModule.pick({UserRoundPlus, CircleUserRound})),
   ],
 };
